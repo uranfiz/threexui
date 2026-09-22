317f2161a9afe9dcc94ea9d12c8c9efb84c81ef4
uranfiz <devdreamix@gmail.com>
2026-09-22 11:43:21 +0300

    Delete src/threexui/__init__.py

--- src/threexui/__init__.py
+++
@@ -1,6 +0,0 @@
-from .panel import Panel
-from .exceptions import ThreeXuiError, AuthError, APIError
-from .models import Client, Inbound, ClientTraffic
-
-__version__ = "0.1.0"
-__all__ = ["Panel", "Client", "Inbound", "ClientTraffic", "ThreeXuiError", "AuthError", "APIError"]
